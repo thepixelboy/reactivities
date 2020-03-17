ccce42db567a1699114b6d6d59617970a15e79e3
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-03-17 19:58:05 +0100

    Adding Semantic-UI to the app

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Header, Icon, List } from 'semantic-ui-react';
 import './App.css';
 import axios from 'axios';
 
@@ -17,15 +17,16 @@ class App extends Component {
 
   render() {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <ul>
-            {this.state.values.map((value: any) => (
-              <li key={value.id}>{value.name}</li>
-            ))}
-          </ul>
-        </header>
+      <div>
+        <Header as='h2'>
+          <Icon name='users' />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+        </List>
       </div>
     );
   }
