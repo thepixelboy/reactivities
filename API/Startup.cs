45082daa325dc6d87aa4b4ed6d945a7dc58090dd
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-03-17 13:02:36 +0100

    Adding the Entity Framework code first migration

--- API/Startup.cs
+++ API/Startup.cs
@@ -29,7 +29,7 @@ namespace API
     {
       services.AddDbContext<DataContext>(opt =>
       {
-        opt.UseSqlite(Configuration.GetConnectionString("Default"));
+        opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
       });
       services.AddControllers();
     }
