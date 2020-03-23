10526f8b5c0763aebb571bac4a20bb062a790946
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-03-23 17:23:55 +0100

    Listing activities

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,11 +1,11 @@
 import axios, { AxiosResponse } from 'axios';
 import { IActivity } from '../models/activity';
 
-axios.defaults.baseURL = 'http://localhost:500/api';
+axios.defaults.baseURL = 'http://localhost:5000/api';
 
 const responseBody = (response: AxiosResponse) => response.data;
 
-const request = {
+const requests = {
   get: (url: string) => axios.get(url).then(responseBody),
   post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
   put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
@@ -13,12 +13,12 @@ const request = {
 };
 
 const Activities = {
-  list: () => request.get('/activities'),
-  details: (id: string) => request.get(`/activities/${id}`),
-  create: (activity: IActivity) => request.post('/activities', activity),
+  list: (): Promise<IActivity[]> => requests.get('/activities'),
+  details: (id: string) => requests.get(`/activities/${id}`),
+  create: (activity: IActivity) => requests.post('/activities', activity),
   update: (activity: IActivity) =>
-    request.put(`/activities/${activity.id}`, activity),
-  delete: (id: string) => request.del(`/activities/${id}`)
+    requests.put(`/activities/${activity.id}`, activity),
+  delete: (id: string) => requests.del(`/activities/${id}`)
 };
 
 export default {
