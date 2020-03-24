541adf2f06730232009a49786381cb53db6cc4b7
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-03-24 15:50:06 +0100

    Adding the Delete Activity action

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -25,19 +25,6 @@ const App = () => {
   const [submitting, setSubmitting] = useState(false);
   const [target, setTarget] = useState('');
 
-  const handleDeleteActivity = (
-    event: SyntheticEvent<HTMLButtonElement>,
-    id: string
-  ) => {
-    setSubmitting(true);
-    setTarget(event.currentTarget.name);
-    agent.Activities.delete(id)
-      .then(() => {
-        setActivities([...activities.filter(a => a.id !== id)]);
-      })
-      .then(() => setSubmitting(false));
-  };
-
   useEffect(() => {
     activityStore.loadActivities();
   }, [activityStore]);
@@ -49,11 +36,7 @@ const App = () => {
     <Fragment>
       <NavBar />
       <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard
-          deleteActivity={handleDeleteActivity}
-          submitting={submitting}
-          target={target}
-        />
+        <ActivityDashboard />
       </Container>
     </Fragment>
   );
