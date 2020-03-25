f7f8c32a3ba05c89545e45bff9711abe1f7059b0
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-03-25 16:24:22 +0100

    Scrolling to the top of the page on navigation

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -4,9 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
+import ScrollToTop from './app/layout/ScrollToTop';
 
 ReactDOM.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>,
   document.getElementById('root')
