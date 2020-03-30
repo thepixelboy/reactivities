5c1fe2d21ba55c9e5b9f4eb189b49327b0e0bb54
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-03-30 10:03:18 +0200

    Creating a reusable Date Input field

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -10,6 +10,7 @@ import { TextInfo } from '../../../app/common/form/TextInfo';
 import { TextAreaInput } from '../../../app/common/form/TextAreaInput';
 import { SelectInput } from '../../../app/common/form/SelectInput';
 import { category } from '../../../app/common/options/categoryOptions';
+import { DateInput } from '../../../app/common/form/DateInput';
 
 interface DetailParams {
   id: string;
@@ -34,7 +35,7 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
     title: '',
     category: '',
     description: '',
-    date: '',
+    date: null,
     city: '',
     venue: ''
   });
@@ -107,8 +108,8 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
                 <Field
                   name='date'
                   placeholder='Date'
-                  value={activity.date}
-                  component={TextInfo}
+                  value={activity.date!}
+                  component={DateInput}
                 />
                 <Field
                   name='city'
