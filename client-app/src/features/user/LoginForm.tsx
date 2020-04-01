4440c67c68dc08d0d382447718257af24a1a2a77
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-04-01 09:55:24 +0200

    Adding Modals to the application

--- client-app/src/features/user/LoginForm.tsx
+++ client-app/src/features/user/LoginForm.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Form as FinalForm, Field } from 'react-final-form';
-import { Form, Button, Label } from 'semantic-ui-react';
+import { Form, Button, Label, Header } from 'semantic-ui-react';
 import { TextInput } from '../../app/common/form/TextInput';
 import { RootStoreContext } from '../../app/stores/rootStore';
 import { IUserFormValues } from '../../app/models/user';
@@ -25,13 +25,18 @@ export const LoginForm = () => {
       render={({
         handleSubmit,
         submitting,
-        form,
         submitError,
         invalid,
         pristine,
         dirtySinceLastSubmit
       }) => (
         <Form onSubmit={handleSubmit}>
+          <Header
+            as='h2'
+            content='Login to Reactivities'
+            color='teal'
+            textAlign='center'
+          />
           <Field name='email' component={TextInput} placeholder='Email' />
           <Field
             name='password'
@@ -42,14 +47,13 @@ export const LoginForm = () => {
           {submitError && !dirtySinceLastSubmit && (
             <Label color='red' basic content={submitError.statusText} />
           )}
-          <br />
           <Button
             disabled={(invalid && !dirtySinceLastSubmit) || pristine}
             loading={submitting}
-            positive
+            color='teal'
             content='Login'
+            fluid
           />
-          <pre>{JSON.stringify(form.getState(), null, 2)}</pre>
         </Form>
       )}
     />
