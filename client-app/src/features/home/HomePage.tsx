0fa166d9640f0a58b62f5c6227efe1addf57bdb8
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-04-01 11:11:50 +0200

    Adding a Register form

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -3,6 +3,7 @@ import { Container, Segment, Header, Button, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { RootStoreContext } from '../../app/stores/rootStore';
 import { LoginForm } from '../user/LoginForm';
+import { RegisterForm } from '../user/RegisterForm';
 
 export const HomePage = () => {
   const rootStore = useContext(RootStoreContext);
@@ -36,13 +37,16 @@ export const HomePage = () => {
             <Header as='h2' inverted content='Welcome to Reactivities' />
             <Button
               onClick={() => openModal(<LoginForm />)}
-              to='/login'
               size='huge'
               inverted
             >
               Login
             </Button>
-            <Button as={Link} to='/register' size='huge' inverted>
+            <Button
+              onClick={() => openModal(<RegisterForm />)}
+              size='huge'
+              inverted
+            >
               register
             </Button>
           </Fragment>
