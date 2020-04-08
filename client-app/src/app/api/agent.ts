8194093704891d0f9cb80934c637eb11796a3e06
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-04-08 10:26:29 +0200

    Hooking it all up to the API

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -7,17 +7,17 @@ import { IUser, IUserFormValues } from '../models/user';
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
 axios.interceptors.request.use(
-  config => {
+  (config) => {
     const token = window.localStorage.getItem('jwt');
     if (token) config.headers.Authorization = `Bearer ${token}`;
     return config;
   },
-  error => {
+  (error) => {
     return Promise.reject(error);
   }
 );
 
-axios.interceptors.response.use(undefined, error => {
+axios.interceptors.response.use(undefined, (error) => {
   if (error.message === 'Network Error' && !error.response) {
     toast.error('Network error - make sure API is running!');
   }
@@ -41,31 +41,17 @@ axios.interceptors.response.use(undefined, error => {
 const responseBody = (response: AxiosResponse | undefined) => response?.data;
 
 const sleep = (ms: number) => (response: AxiosResponse) =>
-  new Promise<AxiosResponse>(resolve =>
+  new Promise<AxiosResponse>((resolve) =>
     setTimeout(() => resolve(response), ms)
   );
 
 const requests = {
-  get: (url: string) =>
-    axios
-      .get(url)
-      .then(sleep(1000))
-      .then(responseBody),
+  get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
   post: (url: string, body: {}) =>
-    axios
-      .post(url, body)
-      .then(sleep(1000))
-      .then(responseBody),
+    axios.post(url, body).then(sleep(1000)).then(responseBody),
   put: (url: string, body: {}) =>
-    axios
-      .put(url, body)
-      .then(sleep(1000))
-      .then(responseBody),
-  del: (url: string) =>
-    axios
-      .delete(url)
-      .then(sleep(1000))
-      .then(responseBody)
+    axios.put(url, body).then(sleep(1000)).then(responseBody),
+  del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody),
 };
 
 const Activities = {
@@ -74,7 +60,9 @@ const Activities = {
   create: (activity: IActivity) => requests.post('/activities', activity),
   update: (activity: IActivity) =>
     requests.put(`/activities/${activity.id}`, activity),
-  delete: (id: string) => requests.del(`/activities/${id}`)
+  delete: (id: string) => requests.del(`/activities/${id}`),
+  attend: (id: string) => requests.post(`/activities/${id}/attend`, {}),
+  unattend: (id: string) => requests.del(`/activities/${id}/attend`),
 };
 
 const User = {
@@ -82,10 +70,10 @@ const User = {
   login: (user: IUserFormValues): Promise<IUser> =>
     requests.post('/user/login', user),
   register: (user: IUserFormValues): Promise<IUser> =>
-    requests.post('/user/register', user)
+    requests.post('/user/register', user),
 };
 
 export default {
   Activities,
-  User
+  User,
 };
