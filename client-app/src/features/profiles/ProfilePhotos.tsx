6703224e392e7f036a1821f89846009f6cb81a05
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-04-15 17:33:58 +0200

    Adding the set main photo functionality

--- client-app/src/features/profiles/ProfilePhotos.tsx
+++ client-app/src/features/profiles/ProfilePhotos.tsx
@@ -11,8 +11,10 @@ const ProfilePhotos = () => {
     isCurrentUser,
     uploadPhoto,
     uploadingPhoto,
+    setMainPhoto,
+    loading,
   } = rootStore.profileStore;
-  const [addPhotoMode, setAddPhotoMode] = useState(true);
+  const [addPhotoMode, setAddPhotoMode] = useState(false);
 
   const handleUploadImage = (photo: Blob) => {
     uploadPhoto(photo).then(() => setAddPhotoMode(false));
@@ -46,7 +48,13 @@ const ProfilePhotos = () => {
                     <Image src={photo.url} />
                     {isCurrentUser && (
                       <Button.Group fluid widths={2}>
-                        <Button basic positive content='Main' />
+                        <Button
+                          onClick={() => setMainPhoto(photo)}
+                          loading={loading}
+                          basic
+                          positive
+                          content='Main'
+                        />
                         <Button basic negative icon='trash' />
                       </Button.Group>
                     )}
