dd8282e0d3317a8d1d59d866c623391a83804237
Ruben <34570952+thepixelboy@users.noreply.github.com>
2024-07-03 19:17:35 +0200

    feat: select an activity using MobX

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -4,7 +4,7 @@ import agent from "../api/agent";
 
 export default class ActivityStore {
   activities: Activity[] = [];
-  selectedActivity: Activity | null = null;
+  selectedActivity: Activity | undefined = undefined;
   editMode = false;
   loading = false;
   loadingInitial = false;
@@ -33,5 +33,23 @@ export default class ActivityStore {
 
   setLoadingInitial = (state: boolean) => {
     this.loadingInitial = state;
+  };
+
+  selectActivity = (id: string) => {
+    this.selectedActivity =
+      this.activities.find((a) => a.id === id) ?? undefined;
+  };
+
+  cancelSelectedActivity = () => {
+    this.selectedActivity = undefined;
+  };
+
+  openForm = (id?: string) => {
+    id ? this.selectActivity(id) : this.cancelSelectedActivity();
+    this.editMode = true;
+  };
+
+  closeForm = () => {
+    this.editMode = false;
   };
 }
