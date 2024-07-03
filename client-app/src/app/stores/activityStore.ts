c6f3100e3edaf7e479ea90746ec903059fb91510
Ruben <34570952+thepixelboy@users.noreply.github.com>
2024-07-03 18:20:37 +0200

    feat: MonX strict mode

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -14,17 +14,24 @@ export default class ActivityStore {
   }
 
   loadActivities = async () => {
-    this.loadingInitial = true;
+    this.setLoadingInitial(true);
     try {
       const activities = await agent.Activities.list();
+
       activities.forEach((activity) => {
         activity.date = activity.date.split("T")[0];
         this.activities.push(activity);
+
+        this.setLoadingInitial(false);
       });
-      this.loadingInitial = false;
     } catch (error) {
       console.log(error);
-      this.loadingInitial = false;
+
+      this.setLoadingInitial(false);
     }
+  };
+
+  setLoadingInitial = (state: boolean) => {
+    this.loadingInitial = state;
   };
 }
