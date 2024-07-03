59f4d9be58713fc3c134c44f84cdb62037ba96ee
Ruben <34570952+thepixelboy@users.noreply.github.com>
2024-07-03 17:07:33 +0200

    feat: setup mobX

---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,11 @@
+import { makeAutoObservable, observable } from "mobx";
+
+export default class ActivityStore {
+  title = "Hello from MobX!";
+
+  constructor() {
+    makeAutoObservable(this, {
+      title: observable,
+    });
+  }
+}
