2a69fa437b7fd5c9cace7431d468d058871cac03
Ruben <34570952+thepixelboy@users.noreply.github.com>
2024-07-14 12:01:21 +0200

    feat: create a reusable date input

---
+++ client-app/src/app/common/form/MyDateInput.tsx
@@ -0,0 +1,21 @@
+import { useField } from "formik";
+import { Form, Label } from "semantic-ui-react";
+import DatePicker, { DatePickerProps } from "react-datepicker";
+
+export default function MyDateInput(props: Partial<DatePickerProps>) {
+  const [field, meta, helpers] = useField(props.name!);
+
+  return (
+    <Form.Field error={meta.touched && !!meta.error}>
+      <DatePicker
+        {...field}
+        {...props}
+        selected={(field.value && new Date(field.value)) || null}
+        onChange={(value) => helpers.setValue(value)}
+      />
+      {meta.touched && meta.error ? (
+        <Label basic color="red" content={meta.error} />
+      ) : null}
+    </Form.Field>
+  );
+}
