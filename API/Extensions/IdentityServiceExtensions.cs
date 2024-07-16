86ab057b38eeaa8bfef486db321e58347eb179b0
Ruben <34570952+thepixelboy@users.noreply.github.com>
2024-07-16 17:39:12 +0200

    feat: configure identity in the Startup class

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddIdentityCore<AppUser>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+                .AddEntityFrameworkStores<DataContext>();
+
+                services.AddAuthentication();
+
+                return services;
+                
+        }
+    }
+}
