0e5b5954fdfb7fb790c1e74391ae9fdf4a11d909
Ruben <34570952+thepixelboy@users.noreply.github.com>
2024-07-24 18:15:26 +0200

    feat: validate the registration of users

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,10 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        
+        [Required]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}", ErrorMessage = "Password must have 1 uppercase, 1 lowercase, 1 number, 1 non alphanumeric and at least 4 characters")]
         public string Password { get; set; }
+        
+        [Required]
         public string DisplayName { get; set; }
+        
+        [Required]
         public string Username { get; set; }
     }
 }
