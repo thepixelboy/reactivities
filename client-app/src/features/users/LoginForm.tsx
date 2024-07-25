224ffdafb03b2a923eba7ba0bea23d07b3ad9dec
Ruben <34570952+thepixelboy@users.noreply.github.com>
2024-07-25 16:44:21 +0200

    feat: display errors in the form

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,6 +1,6 @@
-import { Form, Formik } from "formik";
+import { ErrorMessage, Form, Formik } from "formik";
 import MyTextInput from "../../app/common/form/MyTextInput";
-import { Button } from "semantic-ui-react";
+import { Button, Label } from "semantic-ui-react";
 import { useStore } from "../../app/stores/store";
 import { observer } from "mobx-react-lite";
 
@@ -9,13 +9,28 @@ export default observer(function LoginForm() {
 
   return (
     <Formik
-      initialValues={{ email: "", password: "" }}
-      onSubmit={(values) => userStore.login(values)}
+      initialValues={{ email: "", password: "", error: null }}
+      onSubmit={(values, { setErrors }) =>
+        userStore
+          .login(values)
+          .catch(() => setErrors({ error: "Invalid email or password" }))
+      }
     >
-      {({ handleSubmit, isSubmitting }) => (
+      {({ handleSubmit, isSubmitting, errors }) => (
         <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
           <MyTextInput name="email" placeholder="Email" />
           <MyTextInput name="password" placeholder="Password" type="password" />
+          <ErrorMessage
+            name="error"
+            render={() => (
+              <Label
+                style={{ maringBottom: 10 }}
+                basic
+                color="red"
+                content={errors.error}
+              />
+            )}
+          />
           <Button
             loading={isSubmitting}
             positive
