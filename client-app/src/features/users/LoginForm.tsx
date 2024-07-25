a9adb343166edd63357f7b6ea46640d5311a737c
Ruben <34570952+thepixelboy@users.noreply.github.com>
2024-07-25 16:32:13 +0200

    feat: create a user store

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,20 +1,30 @@
 import { Form, Formik } from "formik";
 import MyTextInput from "../../app/common/form/MyTextInput";
 import { Button } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
+import { observer } from "mobx-react-lite";
 
-export default function LoginForm() {
+export default observer(function LoginForm() {
+  const { userStore } = useStore();
+
   return (
     <Formik
       initialValues={{ email: "", password: "" }}
-      onSubmit={(values) => console.log(values)}
+      onSubmit={(values) => userStore.login(values)}
     >
-      {({ handleSubmit }) => (
+      {({ handleSubmit, isSubmitting }) => (
         <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
           <MyTextInput name="email" placeholder="Email" />
           <MyTextInput name="password" placeholder="Password" type="password" />
-          <Button positive content="Login" type="submit" fluid />
+          <Button
+            loading={isSubmitting}
+            positive
+            content="Login"
+            type="submit"
+            fluid
+          />
         </Form>
       )}
     </Formik>
   );
-}
+});
